1c99ba53003db8dc3c3c0eeb9ed302d4ddc6e91d
GiangNX <nguyen892xg@prestigein.com>
2025-08-07 17:27:45 +0700

    gggg

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+function includeHTML() {
+  const elements = document.querySelectorAll('[data-include]');
+  elements.forEach(async (el) => {
+    const file = el.getAttribute("data-include");
+    try {
+      const resp = await fetch(file);
+      const html = await resp.text();
+      el.innerHTML = html;
+    } catch (err) {
+      el.innerHTML = "Page not found.";
+    }
+  });
+}
+window.addEventListener("DOMContentLoaded", includeHTML);
